1d2ced5e71f97bcdb4f9bd843462353ec8a8873e
yuriyzadorozhny <yuriy.zadorozhny@binance.com>
2022-12-07 14:50:37 +0300

    ipv4 prefix adding when grpc address is build

--- src/common/remote/mod.rs
+++ src/common/remote/mod.rs
@@ -3,9 +3,7 @@ pub mod grpc;
 use crate::api::error::Error::WrongServerAddress;
 use std::sync::atomic::{AtomicI64, Ordering};
 
-use crate::api::error::Result;
-
-const IPV4: &str = "ipv4";
+use crate::api::error::{Error, Result};
 
 // odd by client request id.
 const SEQUENCE_INITIAL_VALUE: i64 = 1;
@@ -22,26 +20,37 @@ pub(crate) fn generate_request_id() -> String {
 
 /// make address's port plus 1000
 pub(crate) fn into_grpc_server_addr(address: &str) -> Result<String> {
-    match address.split_once(':') {
-        None => Err(WrongServerAddress(address.into())),
-        Some((schema, addresses)) if schema.starts_with(IPV4) => {
-            let host_port_pairs = addresses
-                .split(',')
-                .map(|host_port| {
-                    let split = host_port.split(':').collect::<Vec<&str>>();
-                    let host = split.get(0).unwrap();
-                    let port = split.get(1).unwrap().parse::<u32>().unwrap() + 1000;
-                    format!("{host}:{port}")
-                })
-                .collect::<Vec<String>>()
-                .join(",");
+    let hosts = address.split(',').collect::<Vec<&str>>();
+    if hosts.len() == 0 {
+        return Err(WrongServerAddress(address.into()));
+    }
 
-            Ok(format!("{}:{}", schema, host_port_pairs))
+    let mut result = vec![];
+    for host in hosts {
+        let host_port_pair = host.split(':').collect::<Vec<&str>>();
+        if host_port_pair.len() != 2 {
+            return Err(WrongServerAddress(address.into()));
         }
-        Some((host, port)) => {
-            let port = port.parse::<u32>().unwrap() + 1000;
-            Ok(format!("{}:{}", host, port))
+
+        let host = host_port_pair.get(0);
+        let port = host_port_pair.get(1);
+        if host.is_none() || port.is_none() {
+            return Err(WrongServerAddress(address.into()));
         }
+
+        let port = port
+            .unwrap()
+            .parse::<u32>()
+            .map(|port| port + 1000)
+            .map_err(|_| WrongServerAddress(address.into()))?;
+
+        result.push(format!("{}:{}", host.unwrap(), port));
+    }
+
+    match result.len() {
+        0 => Err(WrongServerAddress(address.into())),
+        1 => Ok(format!("{}", result.get(0).unwrap())),
+        _ => Ok(format!("ipv4:{}", result.join(","))),
     }
 }
 
@@ -66,6 +75,14 @@ mod tests {
     }
 
     #[test]
+    fn test_host_addresses_without_one_port() {
+        match into_grpc_server_addr("127.0.0.1:8848,127.0.0.1") {
+            Ok(_) => assert!(false),
+            Err(_) => assert!(true),
+        }
+    }
+
+    #[test]
     fn test_single_host_address() {
         let addr = "127.0.0.1:8848";
         let expected = "127.0.0.1:9848";
@@ -74,16 +91,8 @@ mod tests {
     }
 
     #[test]
-    fn test_single_ipv4_address() {
-        let addr = "ipv4:127.0.0.1:8848";
-        let expected = "ipv4:127.0.0.1:9848";
-        let result = into_grpc_server_addr(addr).unwrap();
-        assert_eq!(expected, result);
-    }
-
-    #[test]
     fn test_multiple_ipv4_address() {
-        let addr = "ipv4:127.0.0.1:8848,127.0.0.1:8849,127.0.0.1:8850";
+        let addr = "127.0.0.1:8848,127.0.0.1:8849,127.0.0.1:8850";
         let expected = "ipv4:127.0.0.1:9848,127.0.0.1:9849,127.0.0.1:9850";
         let result = into_grpc_server_addr(addr).unwrap();
         assert_eq!(expected, result);
