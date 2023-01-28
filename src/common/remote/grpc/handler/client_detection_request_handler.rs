feb52c3701eefada270e703fd78d6e86b6442e9e
onewe <admin@onew.me>
2023-01-28 17:59:48 +0800

    add unit test for ClientDetectionRequestHandler

--- src/common/remote/grpc/handler/client_detection_request_handler.rs
+++ src/common/remote/grpc/handler/client_detection_request_handler.rs
@@ -59,3 +59,52 @@ impl GrpcPayloadHandler for ClientDetectionRequestHandler {
         });
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use tracing::metadata::LevelFilter;
+
+    use super::*;
+
+    pub fn setup() {
+        tracing_subscriber::fmt()
+            .with_thread_names(true)
+            .with_file(true)
+            .with_level(true)
+            .with_line_number(true)
+            .with_thread_ids(true)
+            .with_max_level(LevelFilter::DEBUG)
+            .init();
+    }
+    #[test]
+    pub fn test_payload_convert_error() {
+        setup();
+
+        let target_handler = ClientDetectionRequestHandler;
+
+        let (sender, mut receiver) =
+            tokio::sync::mpsc::channel::<crate::api::error::Result<Payload>>(1024);
+        let response_writer = ResponseWriter::new(sender);
+
+        let request = ClientDetectionRequest {
+            request_id: Some("xd110022131".to_owned()),
+            ..Default::default()
+        };
+
+        let request = GrpcMessageBuilder::new(request).build();
+        let payload = request.into_payload().unwrap();
+
+        target_handler.hand(response_writer, payload);
+
+        let ret_payload = receiver.blocking_recv().unwrap().unwrap();
+
+        let response_message = GrpcMessage::<ClientDetectionResponse>::from_payload(ret_payload);
+
+        let response_message = response_message.unwrap();
+        let response_message = response_message.into_body();
+
+        let request_id = response_message.request_id.unwrap();
+
+        assert_eq!(request_id, "xd110022131");
+    }
+}
