5cc4eacaabfb5f59a68d6461ab5890a0eff4b054
onewe <admin@onew.me>
2023-05-25 14:35:06 +0800

    chore: add some logs

--- src/naming/updater/mod.rs
+++ src/naming/updater/mod.rs
@@ -233,7 +233,7 @@ impl ServiceInfoUpdateTask {
                     continue;
                 }
                 let response = ret.unwrap();
-                debug!("{log_tag}:ServiceInfoUpdateTask query service info response: {response:?}");
+                info!("{log_tag}:ServiceInfoUpdateTask query service info response: {response:?}");
                 if !response.is_success() {
                     let result_code = response.result_code;
                     let error_code = response.error_code;
